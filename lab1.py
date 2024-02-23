b048ddab8b8a85474bbf076ab453964f457d7d07
LC208 <maks.2.03@mail.ru>
2024-02-23 15:17:10 +0800

    combin diff method

--- lab1.py
+++ lab1.py
@@ -3,21 +3,22 @@ import matplotlib.pyplot as pyplot
 import matplotlib as matplotlib
 import numpy as numpy
 
-T = 10
-eps = 0.2
-k = 1
+k = 0.1
+T1 = 0.1
+T2=0.02
 g = 1
 
-def f1(y, z2):
-    return z2-(2*eps*y)/T
+def f1(z):
+    return z
 
-def f2(y):
-    return (k/(T*T))*g - (1/(T*T))*y
+def f2(u,z):
+    return -(T1/(T2*T2))*z - 1/(T2*T2)*u+g
 
 y=0
-z2=0
-dt = T/100
-L = (6*numpy.pi*T)/numpy.sqrt(1+eps*eps)
+u=0
+z=0
+dt = T2/10
+L = 3*T1
 t = 0
 X = list()
 W = list()
@@ -29,16 +30,17 @@ while t < L:
     W.append(w)
     H.append(y)
     one.append(1)
-    k1 = dt*f1(y,z2)
-    q1 = dt*f2(y)
-    k2 = dt*f1(y+k1/2,z2+q1/2)
-    q2 = dt*f2(y+k1/2)
-    k3 = dt*f1(y+k2/2,z2+q2/2)
-    q3 = dt*f2(y+k2/2)
-    k4 = dt*f1(y+k3,z2+q3)
-    q4 = dt*f2(y+k3)
-    z2 = z2 + (q1+2*q2+2*q3+q4)/6
-    y = y + (k1+2*k2+2*k3+k4)/6
+    k1 = dt*f1(z)
+    q1 = dt*f2(u,z)
+    k2 = dt*f1(z+q1/2)
+    q2 = dt*f2(u+k1/2,z+q1/2)
+    k3 = dt*f1(z+q2/2)
+    q3 = dt*f2(u+k2/2,z+q2/2)
+    k4 = dt*f1(z+q3)
+    q4 = dt*f2(u+k3,z+q3)
+    z = z + (q1+2*q2+2*q3+q4)/6
+    u = u + (k1+2*k2+2*k3+k4)/6
+    y = (k*u)/(T2*T2)
     if t > 0:
         w = (H[len(H)-1] - y)/dt
     t+=dt
