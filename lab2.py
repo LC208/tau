8b2c73360542d4a51931601bade035ddf6204ab4
LC208 <maks.2.03@mail.ru>
2024-04-04 14:45:59 +0800

    some small changes

--- lab2.py
+++ lab2.py
@@ -4,11 +4,9 @@ import matplotlib as matplotlib
 from numpy import sin,exp,sqrt,cos, pi
 
 T = 10
-eps = 0.2
+eps = 0.1
 k = 1
 g = 1
-koc = 4 #4
-eps2 = eps - k*koc/(2*T)
 
 def f1(y, z2):
     return z2-(2*eps*y)/T
@@ -16,11 +14,6 @@ def f1(y, z2):
 def f2(y):
     return (k/(T*T))*g - (1/(T*T))*y
 
-def prc(dy):
-    return g + dy*koc
-    
-def nrc(dy):
-    return g - dy*koc
 def r_k(w, y, z2, f1, f2,W,H):
     W.append(w)
     H.append(y)
@@ -37,30 +30,31 @@ def r_k(w, y, z2, f1, f2,W,H):
     w = (y - H[len(H)-1])/dt
     return y,w,z2
 
-y=0
-z2=0
-dt = T/100
-L = (6*pi*T)/sqrt(1+eps*eps)
-t = 0
+
+
 X = list()
 W = list()
 H = list()
 W1 = list()
 H1 = list()
 one = list()
+y=0
+z2=0
 w = 0
 y1=0
 w1=0
 z21=0
-W = list()
-H = list()
+dt = T/100
+L = (6*pi*T)/sqrt(1+eps*eps)
+t = 0
+koc = 2 #4
 while t < L:
     X.append(t)
     one.append(1)
     y1,w1,z21 =r_k(w1,y1,z21,f1,f2,W1,H1)
     g = 1
     y,w,z2 =r_k(w,y,z2,f1,f2,W,H)
-    g = 1 + W1[len(W) - 1]*koc
+    g = 1 - W1[len(W) - 1]*koc
     t+=dt
 pyplot.plot(X, H, color="blue", linewidth = 1, label = "Смоделированная переходная функция")
 pyplot.plot(X, H1, color="red", linewidth = 1, label = "Смоделированная переходная функция с ПОС")
